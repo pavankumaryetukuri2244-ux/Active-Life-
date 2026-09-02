4221ba764d3581fd4f24e0d6655a487f6abf5b13
pavan kumar <pavankumaryetukuri2244@gmail.com>
2026-09-02 18:20:53 +0530

    feat: align stat card icons, update Active Doctors icon and clean legacy APIs

--- src/app/services/webapi.service.ts
+++ src/app/services/webapi.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs/internal/operators/map';
 import { environment } from '../../environments/environment';
 
 @Injectable({
@@ -12,25 +11,6 @@ export class WebapiService {
   constructor(
     public Http: HttpClient
   ) { }
-
-  GetBanners(type: any = '') {
-    const url = `${this.WebApi}/Category/Banners?name=banner?${type}?cache`;
-    return this.Http.get(url, { responseType: "json" }).pipe(map((d: any) => (d.Success === true ? d.Result.Table : false)))
-  }
-
-  public async Login(mobile: any, password: any): Promise<any> {
-    try {
-      return new Promise((resolve, reject) => {
-        const body = { condition: "login", mobile, password }
-        const url = `${this.WebApi}/authentication.php`;
-        return this.Http.post<any>(url, body)
-          .pipe(map((d: any) => (d.status === 1) ? { status: true, data: d.info[0] } : { status: false, error: d.msg }))
-          .subscribe((r: any) => resolve(r), reject);
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  }
 
   public LoginAdmin(body: { email: string; password: string }) {
     const url = `${this.WebApi}/api/v1/auth/login`;
